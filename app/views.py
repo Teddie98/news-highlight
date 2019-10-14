6489d04cf291ff0de59a9c0ff161dd7355f42875
Isaacg94 <7248zack@gmail.com>
2019-10-14 14:19:23 +0300

    create first API call to display a list of sources

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-
+from .request import get_sources
 # Views
 @app.route('/')
 def index():
@@ -8,4 +8,23 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    general_categories = get_sources('general')
+    # business_category = get_sources('business')
+    # entertainment_category = get_sources('entertainment')
+    # sports_category = get_sources('sports')
+    # technology_category = get_sources('technology')
+    # science_category = get_sources('science')
+    # health_category = get_sources('health')
+
+    title = 'World News Highlights'
+    return render_template('index.html',title = title, general = general_categories)
+
+@app.route('/newsarticle/<id>')
+def newsarticle(id):
+
+    '''
+    View article page function that returns the article details page and its data
+    '''
+    # article = get_article
+    title = 'News Articles'
+    return render_template('article.html',title = title,id = id)
